7e5027354b8961d82901a14d89541f330c1070d9
Mohamed Zayan <m7mdzayan@gmail.com>
2020-03-04 19:20:58 +0200

    counter now is ready

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  counter: 0
+};
+
+const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.value
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.value
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
